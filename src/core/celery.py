d86e28b645c0980c3a6f33dab2e0508a2627c42f
Oleh Kazaiev <33457275+TheJog5505@users.noreply.github.com>
2020-09-29 16:18:23 +0300

    Update celery.py

--- src/core/celery.py
+++ src/core/celery.py
@@ -1,16 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from django.conf import settings
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
 
-app = Celery('{{ files.1 }}_celery', broker=settings.BROKER_URL)
-app.config_from_object('celeryconfig')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-# app.autodiscover_tasks()
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app = Celery('{{ files.1 }}_celery')
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks()
 
 if __name__ == '__main__':
     app.start()
