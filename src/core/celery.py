69cd4308c599a1a353c4696c246fc108266b16bc
Oleh Kazaiev <33457275+TheJog5505@users.noreply.github.com>
2020-10-05 11:13:45 +0300

    Update celery.py

--- src/core/celery.py
+++ src/core/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
@@ -8,6 +9,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
 app = Celery('{{ files.1 }}_celery')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+}
 
 if __name__ == '__main__':
     app.start()
