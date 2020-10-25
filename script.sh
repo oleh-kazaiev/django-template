0baad86362dfbb9b1e0f27b25e3e06aa7c0804cb
Oleh Kazaiev <kazaevoleg1997@gmail.com>
2020-10-25 18:36:22 +0200

    fix

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 project_path=`pwd`
 
-test -d {{project_directory|cut:project_name}}{{files.0}} && echo "Directory '{{project_directory|cut:project_name}}{{files.0}}' already exists!" || mv {{project_directory}}/ {{project_directory|cut:project_name}}{{files.0}}/
+rm $project_path/BRAND_NEW/README.md $project_path/BRAND_NEW/script.sh
 
-rm $project_path/README.md $project_path/script.sh
+test -d {{project_directory|cut:project_name}}{{files.0}} && echo "Directory '{{project_directory|cut:project_name}}{{files.0}}' already exists!" || mv {{project_directory}}/ {{project_directory|cut:project_name}}{{files.0}}/
