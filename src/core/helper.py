2f07d9a2a7d6d272c1a5c1fc8278b3a95dfb4174
Oleh Kazaiev <33457275+TheJog5505@users.noreply.github.com>
2021-01-11 10:55:43 +0200

    Update helper.py

--- src/core/helper.py
+++ src/core/helper.py
@@ -2,5 +2,5 @@ from django.conf import settings
 from django.core.mail import send_mail
 
 
-def send_email(subject, message, emails, from_email=None, html_message=None):
-    return send_mail(subject, message, from_email, emails, html_message=html_message, fail_silently=False)
+def send_email(subject, message, emails, from_email=None, html_message=None, fail_silently=False):
+    return send_mail(subject, message, from_email, emails, html_message=html_message, fail_silently=fail_silently)
